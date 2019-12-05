5e2a2305b0cc3686d3144e5cd1f294b5bdc7e1c9
artyom-yurin <artem_yrin@mail.ru>
2019-12-05 14:50:43 +0300

    build of expression
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

---
+++ semantic_analyzer/expressions/expressionProcessing.hpp
@@ -0,0 +1,8 @@
+#ifndef ICOMPILER_EXPRESSIONPROCESSING_HPP
+#define ICOMPILER_EXPRESSIONPROCESSING_HPP
+
+#include "common/Node.hpp"
+
+bool processingExpression(CNode* &parent, int idChild);
+
+#endif // ICOMPILER_EXPRESSIONPROCESSING_HPP
