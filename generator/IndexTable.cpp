75e33558a2812f547f67c0a115b216b2e6e78722
artyom-yurin <artem_yrin@mail.ru>
2019-12-06 11:01:29 +0300

    SOME
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- generator/IndexTable.cpp
+++ generator/IndexTable.cpp
@@ -1,21 +1,18 @@
 #include "IndexTable.hpp"
 
-std::pair<int, Type> IndexTable::getIndex(const std::string &name) {
-    auto index = indexes.find(name);
-    if (index == indexes.end()) {
-        if (parent == nullptr)
-            return -1;
-        return parent->getIndex(name);
-    }
-    return index->second;
+std::pair<int, GeneratorType> IndexTable::getIndex(const std::string &name) {
+  auto index = indexes.find(name);
+  if (index == indexes.end()) {
+    if (parent == nullptr)
+      return std::pair<int, GeneratorType>(-1, GeneratorType::Error);
+    return parent->getIndex(name);
+  }
+  return index->second;
 }
 
-int IndexTable::addIndex(const std::string &name, Type type) {
-    indexes.
-            insert(std::make_pair<std::string, std::pair<int, Type>>(
-            name,
-            std::make_pair<int, Type>(countCurrentVariabels, type))
-    );
-    countCurrentVariabels++;
-    return countCurrentVariabels - 1;
+int IndexTable::addIndex(const std::string &name, GeneratorType type) {
+  indexes.insert(std::pair<std::string, std::pair<int, GeneratorType>>(
+      name, std::pair<int, GeneratorType>(countCurrentVariabels, type)));
+  countCurrentVariabels++;
+  return countCurrentVariabels - 1;
 }
